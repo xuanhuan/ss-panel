20648376e37fbe7d3f28bc65ce9d571ea42865fd
xuanhuan <1372460923@qq.com>
2015-05-17 19:51:51 +0800

    删除 <!-- jQuery 2.1.3 --> <script src="../asset/js/jQuery.min.js"></script> <!-- Bootstrap 3.3.2 JS --> <script src="../asset/js/bootstrap.min.js" type="text/javascript"></script> <!-- SlimScroll --> <script src="../asset/plugins/slimScroll/jquery.slimscroll.min.js" type="text/javascript"></script> <!-- FastClick --> <script src='../asset/plugins/fastclick/fastclick.min.js'></script> <!-- AdminLTE App --> <script src="../asset/js/app.min.js" type="text/javascript"></script> ............

--- admin/_footer.php
+++ admin/_footer.php
@@ -14,23 +14,5 @@
 include_once '../ana.php';
 ?>
 
-<!-- jQuery 2.1.3 -->
-<script src="../asset/js/jQuery.min.js"></script>
-<!-- Bootstrap 3.3.2 JS -->
-<script src="../asset/js/bootstrap.min.js" type="text/javascript"></script>
-<!-- SlimScroll -->
-<script src="../asset/plugins/slimScroll/jquery.slimScroll.min.js" type="text/javascript"></script>
-<!-- FastClick -->
-<script src='../asset/plugins/fastclick/fastclick.min.js'></script>
-<!-- AdminLTE App -->
-<script src="../asset/js/app.min.js" type="text/javascript"></script>
-<!-- 下面加载 dataTables 要用的 js 文件 -->
-<script src="../asset/plugins/datatables/jquery.dataTables.js" type="text/javascript"></script>
-<script src="../asset/plugins/datatables/dataTables.bootstrap.js" type="text/javascript"></script>
-<script type="text/javascript">
-  $(function () {
-    $("#example1").dataTable();
-  });
-</script>
 </body>
 </html>
