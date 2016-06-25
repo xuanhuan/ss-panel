48764c9eba364c45500ef54fe8b941994299f3ff
Dwwwwww <Dwwwwww@users.noreply.github.com>
2016-06-25 10:30:22 +0800

    Delete nodeinfo.php

--- user/nodeinfo.php
+++
@@ -1,8 +0,0 @@
-<?php
-ini_set("display_errors", "On");
-error_reporting(E_ALL | E_STRICT);
-require_once '_main.php';
-$node = new Ss\Node\Node();
-$nodeinfo = $node->get_all_server_info(); 
-echo json_encode($nodeinfo);
-?>
